5851fea8f108f40953eaf72e181a0333476f2ce5
Mikkel Malmberg <mikkel@brnbw.com>
2015-11-24 19:06:16 +0100

    Multiply image script can take gradients

--- multiply-image.sh
+++ multiply-image.sh
@@ -1,15 +1,36 @@
 #!/bin/bash
 
-# usage:
-#   $ multiply-image.sh IMAGE.JPG
+if [ ! $(which convert) ]; then
+  echo "ImageMagick is required"
+  echo "  $ brew install imagemagick"
+  exit 1
+fi
+
+if [ "$#" -ne 4 ]; then
+  echo "usage: multiply-image.sh IMAGE FROM_COLOR TO_COLOR OUTPUT"
+  exit 1
+fi
 
 in="$1"
-out=`basename $in`-result.jpg
+overlay_color="$2"
+gradient_color="$3"
+out="$4"
 
-convert "$in" \
-  -grayscale rec709luma \
-  \( -clone 0 -fill red -draw "color 0,0 reset" \) \
+# Get the size of the input image
+size=$(identify -format '%wx%h' "$in")
+
+# Create a temporary image with just the gradient
+gradient=tmp-gradient.png
+
+convert -size "$size" \
+  gradient:$gradient_color-$overlay_color \
+  "$gradient"
+
+# Multiply the gradient on top of the input image
+composite \
+  -gravity center \
   -compose multiply \
-  -composite \
-  -quality 100% \
-  "$out"
+  "$gradient" "$in" "$out"
+
+# Remove temporary gradient image
+rm "$gradient"
