88e4269cec27f6d7ffe3eb8a7c75fc276bdcc0b1
Mikkel Malmberg <mikkel@brnbw.com>
2015-11-25 11:36:11 +0100

    Fetch posts to Home

--- web/client/components/Home.js
+++ web/client/components/Home.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { Component, PropTypes } from 'react'
 import { Link } from 'react-router'
+import { connect } from 'react-redux'
+import { fetchPosts } from '../actions'
 
-export default function Home () {
-  return <div className='Home'>
-    <Link to='/kitchensink'>Kitchensink</Link>
-  </div>
+const stateToProps = state => ({
+  posts: state.posts.items
+})
+
+class Home extends Component {
+  static propTypes = {
+    posts: PropTypes.arrayOf(PropTypes.object)
+  }
+
+  componentDidMount () {
+    this.props.fetchPosts()
+  }
+
+  render () {
+    const { posts } = this.props
+
+    return <div className='Home'>
+      <Link to='/kitchensink'>Kitchensink</Link>
+      {posts.map(post => (
+        <li>{post.title}</li>
+      ))}
+    </div>
+  }
 }
+
+export default connect(stateToProps, { fetchPosts })(Home)
