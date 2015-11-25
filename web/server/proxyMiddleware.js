f8d7d31114e77ba68d0e2a91a803720df22e227c
Mikkel Malmberg <mikkel@brnbw.com>
2015-11-25 09:53:00 +0100

    Add logging and api errors

--- web/server/proxyMiddleware.js
+++ web/server/proxyMiddleware.js
@@ -2,11 +2,24 @@ import request from 'request'
 
 export default function proxyMiddleware (token) {
   return (req, res, next) => {
+    const url = `https://public-api.wordpress.com/rest/v1.1${req.url}`
+
+    console.log(`PROXY GET: ${url}`)
+
     request({
-      url: `https://public-api.wordpress.com/rest/v1.1${req.url}`,
+      url,
       headers: { 'Authorization': `Bearer ${token}` }
     }, (err, resp, body) => {
-      if (err) { return res.end(err) }
+      if (err) {
+        console.error(err)
+        return res.end(err)
+      }
+
+      if (resp.statusCode < 200 || resp.statusCode > 299) {
+        console.error(resp.statusCode)
+        return res.end(body || '' + resp.statusCode)
+      }
+
       res.end(body)
     })
   }
