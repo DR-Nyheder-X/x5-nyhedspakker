8b442fbdfd938cf7a39091b24e0b8eab183dd328
Mikkel Malmberg <mikkel@brnbw.com>
2015-11-25 11:01:23 +0100

    Extract store creation to function

---
+++ web/client/configureStore.js
@@ -0,0 +1,27 @@
+import reducer from './reducers'
+
+import thunk from 'redux-thunk'
+import { createStore, applyMiddleware, compose } from 'redux'
+
+import createBrowserHistory from 'history/lib/createBrowserHistory'
+import { syncReduxAndRouter } from 'redux-simple-router'
+
+export const history = createBrowserHistory()
+
+export default function configureStore () {
+  const middleware = applyMiddleware(thunk)
+
+  const createStoreWithMiddleware = compose(middleware)
+
+  const store = createStoreWithMiddleware(createStore)(reducer)
+  syncReduxAndRouter(history, store)
+
+  if (module.hot) {
+    module.hot.accept('./reducers', () => {
+      const nextReducer = require('./reducers')
+      store.replaceReducer(nextReducer)
+    })
+  }
+
+  return store
+}
