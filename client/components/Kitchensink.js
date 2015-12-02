281fc8bedf4ea856a146cf7834d9db4dc6085e82
Jacques Mourier <jacquesmourier@gmail.com>
2015-12-02 13:35:37 +0100

    Tag + Tag--blocky components

--- client/components/Kitchensink.js
+++ client/components/Kitchensink.js
@@ -4,9 +4,31 @@ import Tag from './Tag'
 export default class Kitchensink extends Component {
   render () {
     return <div>
-      KITCHENSINK
-      <Tag>Swagtag</Tag>
-      <Tag modifier='red'>Swagtag</Tag>
+      <h2>Tag</h2>
+      <Tag modifier='blue'>Blue</Tag>
+      <Tag modifier='hotBlue'>Hot blue</Tag>
+      <Tag modifier='green'>Green</Tag>
+      <Tag modifier='seaGreen'>Seagreen</Tag>
+      <Tag modifier='red'>Red</Tag>
+      <Tag modifier='hotRed'>Hot red</Tag>
+      <Tag modifier='purple'>Purple</Tag>
+      <Tag modifier='deepPurple'>Deep purple</Tag>
+      <Tag modifier='teal'>Teal</Tag>
+      <Tag modifier='orange'>Orange</Tag>
+      <Tag modifier='magenta'>Magenta</Tag>
+
+      <h2>Tag--blocky</h2>
+      <Tag modifier='blocky blue'>Blue</Tag>
+      <Tag modifier='blocky hotBlue'>Hot blue</Tag>
+      <Tag modifier='blocky green'>Green</Tag>
+      <Tag modifier='blocky seaGreen'>Seagreen</Tag>
+      <Tag modifier='blocky red'>Red</Tag>
+      <Tag modifier='blocky hotRed'>Hot red</Tag>
+      <Tag modifier='blocky purple'>Purple</Tag>
+      <Tag modifier='blocky deepPurple'>Deep purple</Tag>
+      <Tag modifier='blocky teal'>Teal</Tag>
+      <Tag modifier='blocky orange'>Orange</Tag>
+      <Tag modifier='blocky magenta'>Magenta</Tag>
     </div>
   }
 }
--- client/components/Tag.js
+++ client/components/Tag.js
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react'
 import classnames from 'classnames'
 import formatClassModifiers from '../utilities/formatClassModifiers'
+import './Tag.scss'
 
 export default class Tag extends Component {
   static propTypes = {
