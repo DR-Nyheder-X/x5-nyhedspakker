b0d72c416286116a259a31093ddb88316787d14f
Mikkel Malmberg <mikkel@brnbw.com>
2015-12-03 21:51:18 +0100

    Dumbed down nav

--- client/components/EntryPage.js
+++ client/components/EntryPage.js
@@ -2,29 +2,47 @@ import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
 import Entry from './Entry'
 import SwiperComponent from './SwiperComponent'
+import { Link } from 'react-router'
 
 const stateToProps = state => ({
-  entries: state.entries
+  entries: state.entries.items
 })
 
 class EntryPage extends Component {
   static propTypes = {
     params: PropTypes.object,
-    entries: PropTypes.object
+    entries: PropTypes.arrayOf(PropTypes.object)
   }
 
   render () {
-    const { params } = this.props
-    const { items } = this.props.entries
-    const entry = items.find(entry => entry.sys.id === params.id)
-    const selected = items.indexOf(entry)
+    const { entries } = this.props
+    const { id } = this.props.params
+    const entry = entries.find(entry => entry.sys.id === id)
 
-    return <SwiperComponent className='EntryPage' slide={selected}>
-      {items.map(entry => (
-        <Entry key={entry.sys.id} entry={entry} />
-      ))}
-    </SwiperComponent>
+    return <TiledEntries entries={entries} selectedEntry={entry} />
   }
+}
+
+function TiledEntries ({ entries, onSwipe, selectedEntry }) {
+  return <SwiperComponent
+    className='TiledEntries'
+    slide={entries.indexOf(selectedEntry)}
+    onSlideChangeEnd={onSwipe}
+    allowScroll
+    >
+    {entries.map(entry => (
+      <div key={entry.sys.id}>
+        <Link to='/'>INDEX</Link>
+        <Entry entry={entry} />
+      </div>
+    ))}
+  </SwiperComponent>
+}
+
+TiledEntries.propTypes = {
+  entries: PropTypes.arrayOf(PropTypes.object),
+  onSwipe: PropTypes.func,
+  selectedEntry: PropTypes.object
 }
 
 export default connect(stateToProps)(EntryPage)
--- client/components/HomePage.js
+++ client/components/HomePage.js
@@ -24,76 +24,17 @@ class HomePage extends Component {
     dispatch: PropTypes.func
   }
 
-  componentDidMount () {
-    this.swiper = findDOMNode(this).swiper
-  }
-
-  handleSlideChange (swiper) {
-    // const { dispatch } = this.props
-
-    // if (swiper.activeIndex === SWIPER_POSITIONS.index) {
-    //   swiper.slides.off('touchmove', stopPropagation)
-    //   // dispatch(updatePath('/'))
-    // } else if (swiper.activeIndex === SWIPER_POSITIONS.entry) {
-    //   swiper.slides.on('touchmove', stopPropagation)
-    //   const { entries } = this.props
-    //   const entrySwiper = this.refs.entries.swiper
-    //   const entry = entries[entrySwiper.activeIndex]
-    //   if (entry) dispatch(updatePath(`/entries/${entry.sys.id}`))
-    // }
-  }
-
   render () {
     const { entries } = this.props
-    const { id } = this.props.params
 
-    const selectedEntry = id &&
-      entries.find(entry => entry.sys.id === id)
-
-    const handleSlideChange = this.handleSlideChange.bind(this)
-
-    return <SwiperComponent
-      className='HomePage'
-      direction='vertical'
-      onSlideChangeEnd={handleSlideChange}
-      slide={id ? SWIPER_POSITIONS.entry : SWIPER_POSITIONS.index}
-      slideClassName='top-most-slide'
-      onlyExternal
-    >
-      <div key={SWIPER_POSITIONS.index}>
-        {entries.map(entry => (
-          <Link to={`/entries/${entry.sys.id}`} key={entry.sys.id}>
-            <li>{entry.fields.title}</li>
-          </Link>
-        ))}
-      </div>
-      <div key={SWIPER_POSITIONS.entry}>
-        <TiledEntries entries={entries} selectedEntry={selectedEntry} />
-      </div>
-    </SwiperComponent>
+    return <div className='HomePage'>
+      {entries.map(entry => (
+        <Link to={`/entries/${entry.sys.id}`} key={entry.sys.id}>
+          <li>{entry.fields.title}</li>
+        </Link>
+      ))}
+    </div>
   }
-}
-
-function TiledEntries ({ entries, onSwipe, selectedEntry }) {
-  return <SwiperComponent
-    className='TiledEntries'
-    slide={entries.indexOf(selectedEntry)}
-    onSlideChangeEnd={onSwipe}
-    allowScroll
-    >
-    {entries.map(entry => (
-      <div key={entry.sys.id}>
-        <Link to='/'>INDEX</Link>
-        <Entry entry={entry} />
-      </div>
-    ))}
-  </SwiperComponent>
-}
-
-TiledEntries.propTypes = {
-  entries: PropTypes.arrayOf(PropTypes.object),
-  onSwipe: PropTypes.func,
-  selectedEntry: PropTypes.object
 }
 
 export default connect(stateToProps)(HomePage)
