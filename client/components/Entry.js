1ade1b86a9dd72583d27069932b11d59e2e1e7b7
Mikkel Malmberg <mikkel@brnbw.com>
2015-12-17 14:19:16 +0100

    New API structure

--- client/components/Entry.js
+++ client/components/Entry.js
@@ -1,30 +1,32 @@
 import React, { PropTypes } from 'react'
-import Markdown from './Markdown'
 import ArticleHeader from './ArticleHeader'
 import Figure from './Figure'
 import Paragraph from './Paragraph'
+import Quote from './Quote'
 
 export default function Entry ({ entry, pos, modifiers }) {
   console.log(entry)
 
-  const { title, body, hashtag, nyhedensTal, nyhedensTalBeskrivelse, readMore, tekstafsnit2 } = entry.fields
+  const { title, body, hashtag, nyhedensTal, nyhedensTalBeskrivelse, tekstafsnit2, quote, quoteSource } = entry.fields
 
   return <div className='Entry'>
     <ArticleHeader title={title} backgroundImageFilename='blueToRedWelcomeHeader.jpg' pos={pos} hashtag={hashtag} modifiers={modifiers} />
-    <Markdown text={body} />
+    <Paragraph modifiers={modifiers}>{body}</Paragraph>
+    {quote && (
+      <Quote
+        cite={quoteSource}
+        modifiers={modifiers}
+      >{quote}</Quote>
+    )}
     {nyhedensTal && (
       <Figure
         description={nyhedensTalBeskrivelse}
-        modifiers={modifiers}>
-        {nyhedensTal}
-      </Figure>
+        modifiers={modifiers}
+      >{nyhedensTal}</Figure>
     )}
     {tekstafsnit2 && (
-      <Markdown text={tekstafsnit2} />
+      <Paragraph modifiers={modifiers}>{tekstafsnit2}</Paragraph>
     )}
-    <Paragraph modifiers={modifiers}>
-      <a href={readMore}>Læs mere på DR.dk</a>
-    </Paragraph>
   </div>
 }
 
--- client/components/Paragraph.js
+++ client/components/Paragraph.js
@@ -1,23 +1,18 @@
-import React, { Component, PropTypes } from 'react'
-import classnames from 'classnames'
+import React, { PropTypes } from 'react'
 import formatClassModifiers from '../utilities/formatClassModifiers'
 import './Paragraph.scss'
 
-export default class Paragraph extends Component {
-  static propTypes = {
-    children: PropTypes.node,
-    className: PropTypes.string,
-    modifiers: PropTypes.string
-  }
+export default function Paragraph ({children, modifiers, className}) {
+  const cls = formatClassModifiers('Paragraph', modifiers, className)
 
-  render () {
-    const cls = classnames(
-      formatClassModifiers('Paragraph', this.props.modifiers),
-      this.props.className
-    )
-
-    return <p className={cls}>
-      {this.props.children}
-    </p>
-  }
+  return <p className={cls}>
+    {children}
+  </p>
 }
+
+Paragraph.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+  modifiers: PropTypes.string
+}
+
