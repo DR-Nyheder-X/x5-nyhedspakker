0c904954de10dfa3df4e928d1013d5017d7ec91a
Mikkel Malmberg <mikkel@brnbw.com>
2015-12-17 10:47:56 +0100

    Tag modifiers in ArticleHeader

--- client/components/ArticleHeader.js
+++ client/components/ArticleHeader.js
@@ -1,5 +1,4 @@
 import React, { PropTypes } from 'react'
-import classnames from 'classnames'
 import formatClassModifiers from '../utilities/formatClassModifiers'
 import Tag from './Tag'
 import Pager from './Pager'
@@ -9,20 +8,18 @@ export default function ArticleHeader ({
   backgroundImageFilename,
   className,
   hashtag,
-  modifier,
+  modifiers,
   pos,
   title
 }) {
   const baseClass = 'ArticleHeader'
-  const cls = classnames(
-    baseClass, className,
-    formatClassModifiers(modifier))
+  const cls = formatClassModifiers(baseClass, modifiers, className)
 
   return <header className={cls}>
     <div className={`${baseClass}-image`} style={{backgroundImage: `url(/dummy-content/${backgroundImageFilename})`}}></div>
     <div className={`${baseClass}-inner`}>
       <div className={`${baseClass}-tagAndPager`}>
-        <Tag modifiers='blocky hotRed'>{hashtag}</Tag>
+        <Tag modifiers={`blocky ${modifiers}`}>{hashtag}</Tag>
         <Pager total={pos.total} page={pos.page} />
       </div>
 
@@ -37,7 +34,7 @@ ArticleHeader.propTypes = {
   backgroundImageFilename: PropTypes.string,
   className: PropTypes.string,
   hashtag: PropTypes.string.isRequired,
-  modifier: PropTypes.string,
+  modifiers: PropTypes.string,
   pos: PropTypes.object.isRequired,
   title: PropTypes.string.isRequired
 }
