c3d6dbbfd6a1b78ebd7287e02de1e7796aba1c79
Jacques Mourier <jacquesmourier@gmail.com>
2015-12-28 15:04:29 +0100

    DownButton and GalleryButton on ArticleHeader

--- client/components/ArticleHeader.js
+++ client/components/ArticleHeader.js
@@ -5,6 +5,8 @@ import Pager from './Pager'
 import { Link } from 'react-router'
 import './ArticleHeader.scss'
 import './BackButton.scss'
+import './DownButton.scss'
+import './GalleryButton.scss'
 
 export default function ArticleHeader ({
   backgroundImageFilename,
@@ -24,6 +26,7 @@ export default function ArticleHeader ({
         <svg className='Next-arrowRight' width='11' height='22' viewBox='0 0 11 22' xmlns='http://www.w3.org/2000/svg'><path d='M6.674 11.408L.114 2.7 2.508.897l7.668 10.177.322.243-7.824 10.382L.28 19.892l6.394-8.484z' fill='#FFFFFF' fill-rule='evenodd'/></svg>
       </Link>
     </div>
+
     <div className={`${baseClass}-image`} style={{backgroundImage: `url(/dummy-content/${backgroundImageFilename})`}}></div>
     <div className={`${baseClass}-inner`}>
 
@@ -39,6 +42,18 @@ export default function ArticleHeader ({
       <h2 className={`${baseClass}-subHeading`}>
         {subTitle}
       </h2>
+    </div>
+
+    <div className='DownButton'>
+      <Link to='#'>
+        <svg className='Next-arrowRight' width='11' height='22' viewBox='0 0 11 22' xmlns='http://www.w3.org/2000/svg'><path d='M6.674 11.408L.114 2.7 2.508.897l7.668 10.177.322.243-7.824 10.382L.28 19.892l6.394-8.484z' fill='#FFFFFF' fill-rule='evenodd'/></svg>
+      </Link>
+    </div>
+
+    <div className='GalleryButton'>
+      <Link to='#'>
+        <svg width='17' height='21' viewBox='0 0 17 21' xmlns='http://www.w3.org/2000/svg'><g fill='#FFF' fill-rule='evenodd'><path d='M0 0h9v13H0zM11 2h2v13h-2V2zM2 15h11v2H2v-2zM15 6h2v13h-2V6zM6 19h11v2H6v-2z'/></g></svg>
+      </Link>
     </div>
   </header>
 }
