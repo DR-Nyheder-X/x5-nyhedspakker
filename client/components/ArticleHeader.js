97bc69664cc549bf924cccd9497ee14852c7fe0d
Jacques Mourier <jacquesmourier@gmail.com>
2016-01-04 10:50:21 +0100

    proper heights for ArticleHeader + shadow

--- client/components/ArticleHeader.js
+++ client/components/ArticleHeader.js
@@ -21,15 +21,11 @@ export default function ArticleHeader ({
   const cls = formatClassModifiers(baseClass, modifiers, className)
 
   return <header className={cls}>
-    <div className={`${baseClass}-inner`}>
+    <div className={`${baseClass}-inner`} style={{backgroundImage: `url(/dummy-content/${backgroundImageFilename})`}}>
       <div className='BackButton'>
         <Link to='/'>
           <svg className='Next-arrowRight' width='11' height='22' viewBox='0 0 11 22' xmlns='http://www.w3.org/2000/svg'><path d='M6.674 11.408L.114 2.7 2.508.897l7.668 10.177.322.243-7.824 10.382L.28 19.892l6.394-8.484z' fill='#FFFFFF' fill-rule='evenodd'/></svg>
         </Link>
-      </div>
-
-      <div className={`${baseClass}-tiltedCropper`}>
-        <div className={`${baseClass}-image`} style={{backgroundImage: `url(/dummy-content/${backgroundImageFilename})`}}></div>
       </div>
 
         <div className={`${baseClass}-tagAndPager`}>
@@ -44,7 +40,6 @@ export default function ArticleHeader ({
         <h2 className={`${baseClass}-subHeading`}>
           {subTitle}
         </h2>
-
 
       <div className='DownButton'>
         <Link to='#'>
