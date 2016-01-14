9292be0bf8245039c911c1b3e8dd21013015b9ef
Jacques Mourier <jacquesmourier@gmail.com>
2016-01-14 14:22:29 +0100

    fixing clip path issues on ArticleHeader (links not clickable)

--- client/components/ArticleHeader.js
+++ client/components/ArticleHeader.js
@@ -64,17 +64,17 @@ export default class ArticleHeader extends Component {
             {subTitle}
             </h2>
 
-            <div className='DownButton'>
-              <a onClick={this.scrollToContent.bind(this)}>
-                <svg className='Next-arrowRight' width='11' height='22' viewBox='0 0 11 22' xmlns='http://www.w3.org/2000/svg'><path d='M6.674 11.408L.114 2.7 2.508.897l7.668 10.177.322.243-7.824 10.382L.28 19.892l6.394-8.484z' fill='#FFFFFF' fill-rule='evenodd'/></svg>
-              </a>
-            </div>
+          </div>
+          <div className='DownButton'>
+            <a onClick={this.scrollToContent.bind(this)}>
+              <svg className='Next-arrowRight' width='11' height='22' viewBox='0 0 11 22' xmlns='http://www.w3.org/2000/svg'><path d='M6.674 11.408L.114 2.7 2.508.897l7.668 10.177.322.243-7.824 10.382L.28 19.892l6.394-8.484z' fill='#FFFFFF' fill-rule='evenodd'/></svg>
+            </a>
+          </div>
 
-            <div className='GalleryButton'>
-              <Link to='#'>
-                <svg width='17' height='21' viewBox='0 0 17 21' xmlns='http://www.w3.org/2000/svg'><g fill='#FFF' fill-rule='evenodd'><path d='M0 0h9v13H0zM11 2h2v13h-2V2zM2 15h11v2H2v-2zM15 6h2v13h-2V6zM6 19h11v2H6v-2z'/></g></svg>
-              </Link>
-            </div>
+          <div className='GalleryButton'>
+            <Link to='#'>
+              <svg width='17' height='21' viewBox='0 0 17 21' xmlns='http://www.w3.org/2000/svg'><g fill='#FFF' fill-rule='evenodd'><path d='M0 0h9v13H0zM11 2h2v13h-2V2zM2 15h11v2H2v-2zM15 6h2v13h-2V6zM6 19h11v2H6v-2z'/></g></svg>
+            </Link>
           </div>
         </header>
   }
